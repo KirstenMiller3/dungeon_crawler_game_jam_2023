92d29e60e0869a94297878d238af418bda6db99d
Kirsten Miller <kirsten.miller@bossastudios.com>
2023-04-02 19:12:10 +0100

    trying to add success sound

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -24,6 +24,7 @@ public class Door : MonoBehaviour {
 
     private void OnMirrorIsComplete(bool prev, bool cur) {
         if(cur) {
+            AudioManager.instance.Play("solved");
             _completeMirrors++;
         }
 
