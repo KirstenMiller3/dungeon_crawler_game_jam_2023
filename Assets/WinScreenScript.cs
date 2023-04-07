46550a36552eeb1d62db2c9f5072d7d4fa8904b2
Kirsten Miller <kirsten.miller@bossastudios.com>
2023-04-07 11:04:13 +0100

    win screen takes you back to main menu with fade

---
+++ Assets/WinScreenScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinScreenScript : MonoBehaviour
+{
+    [SerializeField] private CanvasGroup _fade;
+
+    private void Start() {
+        _fade.alpha = 1f;
+        _fade.DOFade(0f, 1f);
+    }
+
+    public void OnStart() {
+        _fade.alpha = 1f;
+        _fade.DOFade(1f, 1f).OnComplete(MainMenu);
+    }
+
+    public void MainMenu() {
+        SceneManager.LoadScene("MainMenu");
+    }
+    
+
+}
